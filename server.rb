4a34fd5e7602a13483945426aaaf72af64629762
Ann Hall <anniemaesato@gmail.com>
2016-08-15 14:49:06 -0600

    added player,wallet. modified styles, index, server

--- server.rb
+++ server.rb
@@ -14,3 +14,13 @@ get '/high_low' do
 	@high_low = HighLow.new
 	erb :high_low
 end
+
+get '/player' do
+	@name = params[:name]
+	@amount = params[:amount]
+	erb :player
+end
+
+post '/player' do
+	@player = Player.new(@name, @amount)
+end
